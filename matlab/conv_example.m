8b5a7ef4b9d494a470ca1c97beb97daf8494573b
Ali <A_1377ranjbar@yahoo.com>
2019-02-27 23:52:17 +0330

    Discrete-time convolution tutorial added

---
+++ matlab/conv_example.m
@@ -0,0 +1,11 @@
+clear; clc
+nx = 0 : 5;
+x = ones(size(nx));
+ny = convIndices(nx, nx);   % you need to implement the convIndices function. 
+                            % In this example it will output a row vector with elements [0 .. 10]
+y = conv(x, x);
+stem(ny, y, 'lineWidth', 2)
+% Graph labels
+title('plot of signal $y[n]=x[n]*x[n]$', 'interpreter', 'latex', 'fontSize', 16)
+xlabel('$n$', 'interpreter', 'latex')
+ylabel('$y[n]$', 'interpreter', 'latex')
